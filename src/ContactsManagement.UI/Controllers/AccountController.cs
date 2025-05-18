8aebad6824be2be087dd9cc8d1f9560781b85317
Matin <matinnjt2000@gmail.com>
2025-05-18 13:49:25 +0330

    Add AccountController and registration functionality with validation

---
+++ src/ContactsManagement.UI/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using ContactsManagement.Core.Domain.IdentityEntities;
+using ContactsManagement.Core.DTO.Identities;
+using ContactsManagement.UI.Filters.ActionFilters.Account;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsManagement.UI.Controllers;
+[Route("[controller]")]
+public class AccountController : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+    [Route("[action]")]
+    public IActionResult Register()
+    {
+        return View();
+    }
+    
+    [Route("[action]")]
+    [HttpPost]
+    [AccountSubmitRegisterActionFilter]
+    public async Task<IActionResult> SubmitRegister([FromForm] RegisterDTO registerDTO)
+    {
+        return Json(registerDTO);
+    }
+    public IActionResult Login()
+    {
+        throw new NotImplementedException();
+    }
+}
