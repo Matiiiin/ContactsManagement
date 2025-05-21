301772a462f59deb2540f6ea756deffc27cdaccc
Matin <matinnjt2000@gmail.com>
2025-05-21 15:24:33 +0330

    fix roles seed

---
+++ src/ContactsManagement.Infrastructure/Migrations/20250521115356_FixRoleSeed.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactsManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixRoleSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("815b6a98-bd4e-4c08-9ac1-7c795452e498"),
+                column: "NormalizedName",
+                value: "ADMIN");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("8e2af4f9-5b73-4e1d-9ad8-a83b12b4b397"),
+                column: "NormalizedName",
+                value: "USER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("815b6a98-bd4e-4c08-9ac1-7c795452e498"),
+                column: "NormalizedName",
+                value: "Admin");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("8e2af4f9-5b73-4e1d-9ad8-a83b12b4b397"),
+                column: "NormalizedName",
+                value: "User");
+        }
+    }
+}
